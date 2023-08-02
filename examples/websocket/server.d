bacc2814e9fe58e492240a41b0ea85441e7267f5
andrewlalis <andrewlalisofficial@gmail.com>
2023-08-02 13:56:45 -0400

    Made working prototype!

--- examples/websocket/server.d
+++ examples/websocket/server.d
@@ -9,6 +9,16 @@ import handy_httpd.components.websocket;
 import handy_httpd.handlers.path_delegating_handler;
 import slf4d;
 
+class MyWebSocketHandler : WebSocketMessageHandler {
+    override void handleTextMessage(WebSocketTextMessage msg) {
+        infoF!"Got TEXT: %s"(msg.payload);
+    }
+
+    override void handleCloseMessage(WebSocketCloseMessage msg) {
+        infoF!"Closed: %d, %s"(msg.statusCode, msg.message);
+    }
+}
+
 void main() {
     ServerConfig cfg = ServerConfig.defaultValues();
     cfg.workerPoolSize = 3;
@@ -16,7 +26,7 @@ void main() {
     pdh.addMapping(Method.GET, "", (ref HttpRequestContext ctx) {
         ctx.response.writeBodyString("Index page.");
     });
-    WebSocketHandler handler = new WebSocketHandler();
+    WebSocketHandler handler = new WebSocketHandler(new MyWebSocketHandler());
     pdh.addMapping(Method.GET, "/ws", handler);
     new HttpServer(pdh, cfg).start();
 }
