cf75e548dd9db460e1338a754e65b23798f22dc0
andrewlalis <andrewlalisofficial@gmail.com>
2024-01-19 10:46:04 -0500

    Finished sorted multivalue map, and added Optional type.

--- source/handy_httpd/components/multivalue_map.d
+++ source/handy_httpd/components/multivalue_map.d
@@ -4,43 +4,147 @@
  */
 module handy_httpd.components.multivalue_map;
 
-import std.typecons : Nullable, nullable;
+import handy_httpd.components.optional;
 
 /**
  * A multi-valued mapping, where a key value may map to zero, one, or many
  * values.
  */
-struct MultiValueMap(KeyType, ValueType) {
+struct MultiValueMap(KeyType, ValueType, alias KeySort = (a, b) => a < b) {
+    /// The internal structure used to store each key and set of values.
     static struct Entry {
+        /// The key for this entry.
         KeyType key;
+        /// The list of values associated with this entry's key.
         ValueType[] values;
     }
 
+    /// The internal, sorted array of entries.
     private Entry[] entries;
 
-    private Nullable!Entry getEntry(KeyType k) {
-        foreach (Entry entry; entries) {
-            if (entry.key == k) return nullable(entry);
+    /**
+     * Attempts to get the entry for a given key. Complexity is O(log(keyCount)).
+     * Params:
+     *   k = The key to look for.
+     * Returns: An optional that may contain the entry that was found.
+     */
+    private Optional!Entry getEntry(KeyType k) {
+        if (entries.length == 0) return Optional!Entry.empty();
+        // Simple binary search.
+        size_t startIdx = 0;
+        size_t endIdx = entries.length - 1;
+        while (startIdx <= endIdx) {
+            size_t mid = startIdx + (endIdx - startIdx) / 2;
+            if (entries[mid].key == k) return Optional!Entry.of(entries[mid]);
+            if (KeySort(entries[mid].key, k)) {
+                startIdx = mid + 1;
+            } else {
+                endIdx = mid - 1;
+            }
         }
-        return Nullable!Entry.init;
+        return Optional!Entry.empty();
     }
 
-    ValueType[] get(KeyType k) {
-        // TODO: Use faster method instead!
-        auto entry = getEntry(k);
-        if (entry.isNull()) {
-            return [];
-        }
-        return entry.get().values;
+    /**
+     * Gets the number of unique keys in this map.
+     * Returns: The number of unique keys in this map.
+     */
+    size_t length() {
+        return entries.length;
     }
 
+    /**
+     * Determines if this map contains a value for the given key.
+     * Params:
+     *   k = The key to search for.
+     * Returns: True if at least one value exists for the given key.
+     */
+    bool contains(KeyType k) {
+        Optional!Entry optionalEntry = getEntry(k);
+        return !optionalEntry.isNull && optionalEntry.value.values.length > 0;
+    }
+
+    /**
+     * Gets all values associated with a given key.
+     * Params:
+     *   k = The key to get the values of.
+     * Returns: The values associated with the given key, or an empty array if
+     * no values exist for the key.
+     */
+    ValueType[] getAll(KeyType k) {
+        return getEntry(k).map!(e => e.values).orElse([]);
+    }
+
+    /**
+     * Gets the first value associated with a given key, as per the order in
+     * which the values were inserted.
+     * Params:
+     *   k = The key to get the first value of.
+     * Returns: An optional contains the value, if there is at least one value
+     * for the given key.
+     */
+    Optional!ValueType getFirst(KeyType k) {
+        Optional!Entry optionalEntry = getEntry(k);
+        if (optionalEntry.isNull || optionalEntry.value.values.length == 0) {
+            return Optional!ValueType.empty();
+        }
+        return Optional!ValueType.of(optionalEntry.value.values[0]);
+    }
+
+    /**
+     * Adds a single key -> value pair to the map.
+     * Params:
+     *   k = The key.
+     *   v = The value associated with the key.
+     */
     void add(KeyType k, ValueType v) {
-        auto entry = getEntry(k);
-        if (entry.isNull) {
+        auto optionalEntry = getEntry(k);
+        if (optionalEntry.isNull) {
             entries ~= Entry(k, [v]);
+            import std.algorithm.sorting : sort;
+            sort!((a, b) => KeySort(a.key, b.key))(entries);
+        } else {
+            optionalEntry.value.values ~= v;
         }
+    }
+
+    /**
+     * Gets this multivalue map as an associative array, where each key is
+     * mapped to a list of values.
+     * Returns: The associative array.
+     */
+    ValueType[][KeyType] asAssociativeArray() {
+        ValueType[][KeyType] aa;
+        foreach (Entry entry; entries) {
+            aa[entry.key] = entry.values.dup;
+        }
+        return aa;
+    }
+
+    // OPERATOR OVERLOADS below here
+
+    /**
+     * Convenience overload to get the first value for a given key. Note: this
+     * will throw an exception if no values exist for the given key. To avoid
+     * this, use `getFirst` and deal with the missing value yourself.
+     * Params:
+     *   key = The key to get the value of.
+     * Returns: The first value for the given key.
+     */
+    ValueType opIndex(KeyType key) {
+        import std.conv : to;
+        return getFirst(key).orElseThrow("No values exist for key " ~ key.to!string ~ ".");
     }
 }
 
 /// The string => string mapping is a common usecase, so an alias is defined.
 alias StringMultiValueMap = MultiValueMap!(string, string);
+
+unittest {
+    StringMultiValueMap m;
+    m.add("a", "hello");
+    assert(m.getFirst("a").orElseThrow == "hello");
+    m.add("b", "bye");
+    assert(m.getFirst("b").orElseThrow == "bye");
+    assert(m.asAssociativeArray == ["a": ["hello"], "b": ["bye"]]);
+}
---
+++ source/handy_httpd/components/optional.d
@@ -0,0 +1,105 @@
+/**
+ * Modules that defines an `Optional` type, which is a simplified version of
+ * Phobos' Nullable, that also supports mapping the underlying data.
+ */
+module handy_httpd.components.optional;
+
+import std.typecons : Nullable;
+
+/**
+ * A simple wrapper around a value to make it optionally present.
+ */
+struct Optional(T) {
+    /// The internal value of this optional.
+    T value;
+
+    /// Whether this optional is empty.
+    bool isNull = true;
+
+    /**
+     * Constructs an optional value using a given value.
+     * Params:
+     *   value = The value to use.
+     * Returns: An optional that contains the given value.
+     */
+    static Optional!T of(T value) {
+        return Optional!T(value, false);
+    }
+
+    /**
+     * Constructs an optional value using a Phobos nullable.
+     * Params:
+     *   nullableValue = The nullable value to use.
+     * Returns: An optional that contains the given nullable value.
+     */
+    static Optional!T of (Nullable!T nullableValue) {
+        if (nullableValue.isNull) return Optional!T.empty();
+        return Optional!T.of(nullableValue.get);
+    }
+
+    /**
+     * Constructs an optional that's empty.
+     * Returns: An optional that is empty.
+     */
+    static Optional!T empty() {
+        return Optional!T(T.init, true);
+    }
+
+    /**
+     * Converts this optional to a Phobos-style Nullable.
+     * Returns: A `Nullable!T` representing this optional.
+     */
+    Nullable!T asNullable() {
+        Nullable!T n;
+        if (!this.isNull) {
+            n = this.value;
+        }
+        return n;
+    }
+
+    /**
+     * Gets the value of this optional if it exists, otherwise uses a given
+     * default value.
+     * Params:
+     *   defaultValue = The value to return if no default value exists.
+     * Returns: The value of the optional, or the default value if this
+     * optional is empty.
+     */
+    T orElse(T defaultValue) {
+        if (this.isNull) return defaultValue;
+        return this.value;
+    }
+
+    /**
+     * Gets the value of this optional if it exists, or throws an exception.
+     * Params:
+     *   msg = A message to put in the exception.
+     * Returns: The value of this optional.
+     */
+    T orElseThrow(string msg = "Optional value is null.") {
+        if (this.isNull) throw new Exception(msg);
+        return this.value;
+    }
+}
+
+/**
+ * Maps the value of a given optional to another type using a given function.
+ * Params:
+ *   opt = The optional to map.
+ * Returns: An optional whose type is the return-type of the given `fn`
+ * template argument function.
+ */
+auto map(alias fn, T)(Optional!T opt) {
+    alias U = typeof(fn(T.init));
+    if (opt.isNull) return Optional!U.empty();
+    return Optional!U.of(fn(opt.value));
+}
+
+unittest {
+    Optional!string s = Optional!string.of("hello");
+    assert(!s.isNull);
+    assert(s.value == "hello");
+    Optional!int mapped = s.map!(str => 1);
+    assert(!mapped.isNull);
+    assert(mapped.value == 1);
+}
