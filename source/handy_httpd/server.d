1de572bca1ffd6304f0bde6623826bd8155ff346
andrewlalis <andrewlalisofficial@gmail.com>
2024-01-25 13:15:25 -0500

    Try disabling blocking worker pool for windows compat.

--- source/handy_httpd/server.d
+++ source/handy_httpd/server.d
@@ -110,7 +110,7 @@ class HttpServer {
         this.address = parseAddress(config.hostname, config.port);
         this.handler = handler;
         this.exceptionHandler = new BasicServerExceptionHandler();
-        this.requestWorkerPool = new LegacyWorkerPool(this);
+        this.requestWorkerPool = new DistributingWorkerPool(config.receiveBufferSize, config.workerPoolSize);
         if (config.enableWebSockets) {
             this.websocketManager = new WebSocketManager();
         }
