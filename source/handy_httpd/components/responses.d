bf6d8d4b568cf5389af7f47b9fbd29baf30ab4bd
andrewlalis <andrewlalisofficial@gmail.com>
2024-06-28 22:18:03 -0400

    Included mixin-generated response functions in deprecation.

--- source/handy_httpd/components/responses.d
+++ source/handy_httpd/components/responses.d
@@ -36,68 +36,68 @@ deprecated("Use methods available directly from HttpResponse instead.") {
     void respond(ref HttpResponse response, HttpStatus status) {
         respond(response, status, null);
     }
-}
 
-/** 
- * Formats a response function name into a camelCase name that's suitable for
- * use in mixin-generated code.
- * Params:
- *   name = The name of an HTTP status enum.
- * Returns: The formatted function name.
- */
-private string formatResponseFunctionName(string name) {
-    import std.string : toLower, capitalize, split, join;
-    string[] parts = split(toLower(name), "_");
-    string functionName = parts[0];
-    for (size_t i = 1; i < parts.length; i++) {
-        functionName ~= capitalize(parts[i]);
+    /** 
+    * Formats a response function name into a camelCase name that's suitable for
+    * use in mixin-generated code.
+    * Params:
+    *   name = The name of an HTTP status enum.
+    * Returns: The formatted function name.
+    */
+    private string formatResponseFunctionName(string name) {
+        import std.string : toLower, capitalize, split, join;
+        string[] parts = split(toLower(name), "_");
+        string functionName = parts[0];
+        for (size_t i = 1; i < parts.length; i++) {
+            functionName ~= capitalize(parts[i]);
+        }
+        return functionName ~ "Response";
     }
-    return functionName ~ "Response";
-}
 
-// Statically generate simple helper functions for each HTTP status.
-import std.traits : EnumMembers;
+    // Statically generate simple helper functions for each HTTP status.
+    import std.traits : EnumMembers;
 
-// Generates functions like the following:
-// okResponse(ref HttpResponse response) {...}
-// okResponse(ref HttpResponse, string bodyContent, string bodyContentType) {...}
-// notFoundResponse(ref HttpResponse response) {...}
-// notFoundResponse(ref HttpResponse, string bodyContent, string bodyContentType) {...}
-static foreach (member; EnumMembers!HttpStatus) {
-    import std.string : format;
-    mixin(format(
-        q{
-            void %s(ref HttpResponse response) {
-                response.setStatus(HttpStatus.%s);
-            }
+    // Generates functions like the following:
+    // okResponse(ref HttpResponse response) {...}
+    // okResponse(ref HttpResponse, string bodyContent, string bodyContentType) {...}
+    // notFoundResponse(ref HttpResponse response) {...}
+    // notFoundResponse(ref HttpResponse, string bodyContent, string bodyContentType) {...}
+    static foreach (member; EnumMembers!HttpStatus) {
+        import std.string : format;
+        mixin(format(
+            q{
+                void %s(ref HttpResponse response) {
+                    response.setStatus(HttpStatus.%s);
+                }
 
-            void %s(ref HttpRequestContext ctx) {
-                ctx.response.setStatus(HttpStatus.%s);
-            }
-            
-            void %s(
-                ref HttpResponse response,
-                string bodyContent,
-                string bodyContentType = "text/plain; charset=utf-8"
-            ) {
-                response.setStatus(HttpStatus.%s);
-                response.writeBodyString(bodyContent, bodyContentType);
-            }
+                void %s(ref HttpRequestContext ctx) {
+                    ctx.response.setStatus(HttpStatus.%s);
+                }
+                
+                void %s(
+                    ref HttpResponse response,
+                    string bodyContent,
+                    string bodyContentType = "text/plain; charset=utf-8"
+                ) {
+                    response.setStatus(HttpStatus.%s);
+                    response.writeBodyString(bodyContent, bodyContentType);
+                }
 
-            void %s(
-                ref HttpRequestContext ctx,
-                string bodyContent,
-                string bodyContentType = "text/plain; charset=utf-8"
-            ) {
-                ctx.response.setStatus(HttpStatus.%s);
-                ctx.response.writeBodyString(bodyContent, bodyContentType);
-            }
-        },
-        formatResponseFunctionName(__traits(identifier, member)), member,
-        formatResponseFunctionName(__traits(identifier, member)), member,
-        formatResponseFunctionName(__traits(identifier, member)), member,
-        formatResponseFunctionName(__traits(identifier, member)), member
-    ));
+                void %s(
+                    ref HttpRequestContext ctx,
+                    string bodyContent,
+                    string bodyContentType = "text/plain; charset=utf-8"
+                ) {
+                    ctx.response.setStatus(HttpStatus.%s);
+                    ctx.response.writeBodyString(bodyContent, bodyContentType);
+                }
+            },
+            formatResponseFunctionName(__traits(identifier, member)), member,
+            formatResponseFunctionName(__traits(identifier, member)), member,
+            formatResponseFunctionName(__traits(identifier, member)), member,
+            formatResponseFunctionName(__traits(identifier, member)), member
+        ));
+    }
 }
 
 /** 
